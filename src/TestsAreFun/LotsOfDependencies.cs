d6ef7e033c7e16d9eb0742fa7c672f71f57bdfd7
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2022-04-30 23:20:43 +0200

    test: add automock

---
+++ src/TestsAreFun/LotsOfDependencies.cs
@@ -0,0 +1,27 @@
+﻿using TestsAreFun.AgeCalculator;
+using TestsAreFun.Configuration;
+using TestsAreFun.TestDoubleCustomer;
+
+namespace TestsAreFun
+{
+    public class LotsOfDependencies
+    {
+        private readonly IDateTimeProvider _dateTimeProvider;
+        private readonly IConfigurationWrapper _configurationWrapper;
+        private readonly ICustomerValidator _customerValidator;
+        private readonly ICustomerRepository _customerRepository;
+
+        public LotsOfDependencies(IDateTimeProvider dateTimeProvider,
+            IConfigurationWrapper configurationWrapper,
+            ICustomerValidator customerValidator,
+            ICustomerRepository customerRepository)
+        {
+            _dateTimeProvider = dateTimeProvider;
+            _configurationWrapper = configurationWrapper;
+            _customerValidator = customerValidator;
+            _customerRepository = customerRepository;
+        }
+
+        public DateTime GetCurrentDateTime() => _dateTimeProvider.GetDateTime();
+    }
+}
---
+++ tests/TestsAreFun.Tests.Unit/08_Mocking/09_AutoMocking/Automock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AutoMoq;
+using TestsAreFun.AgeCalculator;
+
+namespace TestsAreFun.Tests.Unit._08_Mocking._09_AutoMocking;
+
+public class Automock
+{
+    [Test]
+    public void mock_dependencies_automatically()
+    {
+        var moqer = new AutoMoqer();
+
+        moqer.GetMock<IDateTimeProvider>()
+             .Setup(x => x.GetDateTime())
+             .Returns(new DateTime(2010, 2, 1));
+
+        var lotsOfDependencies = moqer.Create<LotsOfDependencies>();
+
+        var currentDateTime = lotsOfDependencies.GetCurrentDateTime();
+
+        currentDateTime.Should().HaveYear(2010).And.HaveMonth(2).And.HaveDay(1);
+    }
+}
