5993107c97c0936439cdd16f502982237f3cac97
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2022-04-30 23:28:27 +0200

    test: add integration tests

---
+++ tests/TestsAreFun.Tests.Integration/ParametrizedTests/Log2NTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestsAreFun.Tests.Integration.ParametrizedTests;
+
+public class Log2NTests
+{
+    [TestCaseSource(typeof(Log2NData), nameof(Log2NData.Data))]
+    public void Log2N(double a, double expectedResult)
+    {
+        double result = Math.Log(a, newBase: 2);
+
+        Assert.That(result, Is.EqualTo(expectedResult).Within(0.000000000001));
+    }
+}
