579058533449052be23f6616b070ce8fcb30682e
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2022-05-02 16:58:42 +0200

    test: add UsersApiTests

---
+++ tests/TestsAreFun.Tests.Integration/ApiTests/Config.cs
@@ -0,0 +1,7 @@
+﻿namespace TestsAreFun.Tests.Integration.ApiTests;
+
+public static class Config
+{
+    public static string ApiUrl => "https://626ff06fc508beec488c91ed.mockapi.io/api/v1/";
+    public static string UsersResource => "users";
+}
---
+++ tests/TestsAreFun.Tests.Integration/ApiTests/UsersApiTests.cs
@@ -0,0 +1,19 @@
+﻿using Flurl;
+using Flurl.Http;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using FluentAssertions;
+using static TestsAreFun.Tests.Integration.ApiTests.Config;
+
+namespace TestsAreFun.Tests.Integration.ApiTests;
+
+public class UsersApiTests
+{
+    [Test]
+    public async Task get_returns_success()
+    {
+        var rs = await ApiUrl.AppendPathSegment(UsersResource).GetAsync();
+
+        rs.StatusCode.Should().Be(200);
+    }
+}
