085cd6e009463677171c1fb3737b02913cf7be69
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2022-05-24 20:47:31 +0200

    test: add multi-targeted tests

---
+++ tests/TestsAreFun.Tests.Unit.Multitargeted/TimezoneTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FakeTimeZone;
+using NUnit.Framework;
+
+namespace TestsAreFun.Tests.Unit.Multitargeted
+{
+    public class TimezoneTests
+    {
+        [Test]
+        public void TimezoneCanBeFaked()
+        {
+            using (new FakeLocalTimeZone(TimeZoneInfo.FindSystemTimeZoneById("UTC+12")))
+            {
+                var localDateTime = new DateTime(2020, 12, 31, 23, 59, 59, DateTimeKind.Local);
+                var utcDateTime = TimeZoneInfo.ConvertTimeToUtc(localDateTime);
+
+                Assert.Multiple(() =>
+                {
+                    Assert.That(TimeZoneInfo.Local.Id, Is.EqualTo("UTC+12"));
+                    Assert.That(localDateTime, Is.EqualTo(utcDateTime.AddHours(12)));
+                });
+            }
+        }
+    }
+}
